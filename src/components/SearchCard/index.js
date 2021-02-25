2c0fb8d26f1ec094654c6309a0bd9fcb7cf9d2e1
Joao Ficanha <joao.ficanha@sbsistemas.com.br>
2021-02-25 20:53:54 -0300

    fix: remove arquivos de testes temporariamente

--- src/components/SearchCard/index.js
+++ src/components/SearchCard/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 //UI
-import { Grid, Typography, CircularProgress } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import * as styled from './index.style';
